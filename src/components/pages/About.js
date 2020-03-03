b74e7277eaac1156069d1893707bf8e0b659e3b6
JG <noreply@jg.com>
2020-03-02 21:09:48 -0800

    fix type on about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -31,9 +31,9 @@ function About() {
           <Row className="about-text">
             <Col></Col>
             <Col xs={8}>
-              <p> Hi, I’m Julie and I am creative and passionate about turning great ideas into tangible software.</p>
+              <p>Hi, I’m Julie and I am creative and passionate about turning great ideas into tangible software.</p>
 
-              <p>  After earning a degree in Computer Science, I worked at corporations and startyos developing and leading Technical projects. At Verizon and Humana I lead major initiatives in transforming both technical platforms and processes to improve Software Quality. I worked closely with the Sofware Architects, vendors, QA and Business. </p>
+              <p>After earning a degree in Computer Science, I worked at corporations and startups developing and leading Technical projects. At Verizon and Humana I lead major initiatives in transforming both technical platforms and processes to improve Software Quality. I worked closely with the Sofware Architects, vendors, QA and Business. </p>
 
               <p>   I am a recent graduate of the University of Washington full-time Web Development Programming Bootcamp. I have the foundational knowledge of a full-stack developer (MERN), prior experience in leading technical projects, and experience as in QA, which gives me a unique perspective on managing technical projects.  I enjoy working with teams on throughout the full project lifecyle and the pride that comes from delivering a quality product.  I look forward to bringing my professional and educational experiences to my next employer.</p>
             </Col>
