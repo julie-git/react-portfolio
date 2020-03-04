05784f018531ea45f99c538eba86f799897989d1
JG <noreply@jg.com>
2020-03-04 15:23:31 -0800

    added projects to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -10,6 +10,7 @@ import RAD from "../../images/RAD.jpg";
 import Parlor from "../../images/parlorflix.jpg";
 import Pup from "../../images/pupmatch1200_750.jpg";
 import Weather from "../../images/weatherdashboard.jpg";
+import Emp from "../../images/emp_mgmt_cms.jpg"
 
 
 
@@ -79,8 +80,22 @@ function Portfolio() {
                                     <h4>Handyman R.A.D Rapid Automated Dispatch</h4>
                                     <h6>Application for Scheduling Service Providers and Customers to Schedule Handyman Services</h6>
                                     <h6>CRUD application in Handlebars, MySQL, Sequilize, HTML, CSS, Heroku</h6>
-                                    <a href="https://github.com/julie-git/Handyman-RAD" target="_blank"><i class="fab fa-github"></i></a>
-                                    <a href="https://rad-prototype.herokuapp.com/"  target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+                                    <a href="https://github.com/aritse/rad-project" target="_blank"><i class="fab fa-github"></i></a>
+                                    <a href="https://handyman-rad.herokuapp.com/"  target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i></a>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={Emp}
+                                    alt="Employee Management System"
+                                />
+                            <Carousel.Caption>
+                                    <h4>Team Profile Generator CMS</h4>
+                                    <h6>Node.js CLI CMS application for creating an employee org chart</h6>
+                                    <h6>Node.js, HTML, CSS, Bootstrap, Inquirer</h6>
+                                    <a href="https://github.com/julie-git/teamprofilegen" target="_blank"><i class="fab fa-github"></i></a>
+                                    <a href="https://github.com/julie-git/teamprofilegen/blob/master/devteamgen.gif"  target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i></a>
                                 </Carousel.Caption>
                             </Carousel.Item>
                         </Carousel>
